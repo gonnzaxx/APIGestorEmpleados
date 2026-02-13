64473aaaf77c843291b2e3f247c5b2952d67443b
gonnzaxx <gonzalo.santiago@alumno.iesluisvives.org>
2026-02-13 17:55:53 +0100

    Commit inicial

--- app/main.py
+++ app/main.py
@@ -7,12 +7,12 @@ from app.routers import employees, departments
 app = FastAPI()
 
 
-# Registrar las rutas
+
 app.include_router(departments.router)
 app.include_router(employees.router)
 
 
-# Crear tablas en la base de datos si no existen
+
 def init_db():
     SQLModel.metadata.create_all(engine)
 
